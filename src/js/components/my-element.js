4a8259f78149cdd99dc4c20d6cbbae9e05f0d84b
Pesanweb Id <andy.yuli@gmail.com>
2024-08-22 00:15:34 +0700

    buat json file, npx lit-localize extract

--- src/js/components/my-element.js
+++ src/js/components/my-element.js
@@ -1,5 +1,28 @@
-import { LitElement } from 'lit';
+import { html, LitElement } from 'lit';
+import { msg, str } from '@lit/localize';
 
-class MyElement extends LitElement {}
+class MyElement extends LitElement {
+  static properties = {
+    name: {},
+  };
+
+  constructor() {
+    super();
+    this.name = 'John Doe';
+  }
+
+  render() {
+    return html`
+      <h1>${msg(str`Welcome, ${this.name}!`)}</h1>
+      <p>${msg(str`I hope you are in good condition`)}</p>
+      <p>
+        ${msg(html`
+          Please visit <a href="https://www.dicoding.com/" target="_blank">this site</a> to be
+          professional programmer
+        `)}
+      </p>
+    `;
+  }
+}
 
 customElements.define('my-element', MyElement);
